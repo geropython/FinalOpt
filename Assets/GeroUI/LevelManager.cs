674cee50f9c2501140ad9540a6edee571979d90b
geropython <72422102+geropython@users.noreply.github.com>
2023-07-06 19:25:37 -0300

    menu, splash, pausa

---
+++ Assets/GeroUI/LevelManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject _splash;
+    public void PlayGame()
+    {
+        if (Input.GetKey(KeyCode.Space)) SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        //Menu
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _splash.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
