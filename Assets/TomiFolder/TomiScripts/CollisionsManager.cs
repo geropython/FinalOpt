e1781756330627345d2d970522c2ecf0192ede7a
Maxi Perez <55637107+Durzdor@users.noreply.github.com>
2023-07-12 12:51:39 -0300

    Squashed commit of the following:
    commit ced673654084e9b3923cff39bab9fc4d653e505e
    Author: Tomi <63859937+tomicancheff@users.noreply.github.com>
    Date:   Wed Jul 12 12:44:38 2023 -0300
    
        Added new PhysicsObject Script
    
        Moved tomi prefabs to PREFABS folder in assets.
    
        ADDED two new sounds
    
        ADDED few scripts.
    
    commit 8814959089321fc9df45ae26c52fcdb1faee3fd4
    Author: Tomi <63859937+tomicancheff@users.noreply.github.com>
    Date:   Mon Jul 10 16:56:48 2023 -0300
    
        New game Manager changes
    
        ADDED UI Win and lose panels to the new tomi scene (gero)
    
        ADDED new bricksremaining in game manager to keep track of the progress.
    
    commit 949c970f46546403f4c46e37858d1b2d75a75a54
    Author: Tomi <63859937+tomicancheff@users.noreply.github.com>
    Date:   Mon Jul 10 16:11:34 2023 -0300
    
        Primeras Optimizaciones
    
        -Changes in some materials and LIGHTNING
    
        -STATIC Objects in all the Scene
    
        - texture baker added to hierarchy
    
        - 35 BATCHES in play mode.

---
+++ Assets/TomiFolder/TomiScripts/CollisionsManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionsManager : MonoBehaviour
+{
+   //For managing collisions between objects and not overload the gameManager ?¿
+}
---
+++ Assets/TomiFolder/TomiScripts/PhysicsObject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    [SerializeField] private Vector2 position;
+    [SerializeField] private Vector2 velocity;
+    [SerializeField] private Vector2 acceleration;
+    [SerializeField] private float mass;
+
+    public PhysicsObject(Vector2 position, float mass)
+    {
+        this.position = position;
+        this.mass = mass;
+    }
+
+    public void AddForce(Vector2 force)
+    {
+        acceleration += force / mass;
+    }
+
+    public void Update()
+    {
+        Vector2 deltaTime = default;
+        velocity += acceleration * deltaTime;
+        position += velocity * deltaTime;
+        acceleration = Vector2.zero;
+    }
+    
+    
+}
